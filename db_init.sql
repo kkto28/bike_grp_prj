f6c99ba50633a00135bd427d2a12938314876b10
Kei Kuan To <2498456t@student.gla.ac.uk>
2019-10-10 21:17:46 +0100

    revised

--- db_init.sql
+++ db_init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS "users" (
-	"cust_id"	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+	"user_id"	INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
 	"email"	INTEGER NOT NULL UNIQUE,
 	"phone_number"	INTEGER,
 	"name"	TEXT,
@@ -16,21 +16,23 @@ CREATE TABLE IF NOT EXISTS "user_type" (
 
 CREATE TABLE IF NOT EXISTS "orders" (
 	"order_id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"cust_id"	INTEGER NOT NULL,
+	"user_id"	INTEGER NOT NULL,
 	"bike_id"	INTEGER NOT NULL,
 	"start_datetime"	TEXT NOT NULL,
-	"end_datetime"	INTEGER,
+	"end_datetime"	TEXT,
 	"amount"	REAL DEFAULT 0,
 	FOREIGN KEY("bike_id") REFERENCES "bikes"("bike_id"),
-	FOREIGN KEY("cust_id") REFERENCES "users"("cust_id")
+	FOREIGN KEY("user_id") REFERENCES "users"("user_id")
 );
 
 CREATE TABLE IF NOT EXISTS "bikes" (
 	"bike_id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
 	"status"	TEXT NOT NULL,
-	"bike_station"	INTEGER,
-	"defect_id"	INTEGER
-);
+	"parked_bike_station"	INTEGER,
+	"loc_lat"	INTEGER,
+	"loc_long"	INTEGER,
+	FOREIGN KEY("parked_bike_station") REFERENCES "bike_stations"("station_id")
+)
 
 CREATE TABLE IF NOT EXISTS "bike_stations" (
 	"station_id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
@@ -42,10 +44,21 @@ CREATE TABLE IF NOT EXISTS "bike_stations" (
 
 CREATE TABLE IF NOT EXISTS "defect_report" (
 	"report_id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
-	"cust_id"	INTEGER NOT NULL,
+	"user_id"	INTEGER NOT NULL,
+	"bike_id"	INTEGER NOT NULL,
 	"category"	INTEGER NOT NULL,
 	"details"	BLOB,
 	"report_datetime"	TEXT NOT NULL,
 	"status"	TEXT NOT NULL,
-	FOREIGN KEY("cust_id") REFERENCES "users"("cust_id")
+	FOREIGN KEY("user_id") REFERENCES "users"("user_id"),
+	FOREIGN KEY("bike_id") REFERENCES "bikes"("bike_id")
+);
+
+CREATE TABLE IF NOT EXISTS "accounts" (
+	"user_id"	INTEGER NOT NULL PRIMARY KEY UNIQUE,
+	"card_info"	TEXT,
+	"total_amount"	REAL NOT NULL DEFAULT 0,
+	"last_topup_datetime"	TEXT,
+	"last_topup_amount"	REAL,
+	FOREIGN KEY("user_id") REFERENCES "users"("user_id")
 );
